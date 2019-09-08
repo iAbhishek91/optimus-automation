85bb2153525f61cc86334e5d4eabee9b0e26b64b
Abhishek Das <abhi.das2007das@gmail.com>
2019-09-08 11:48:52 +0100

    fix(transpilation): entry point changed to dist/index dist folder added in version control

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _yargs = require("yargs");
+
+var _default = function _default() {
+  return "Argument passed: ".concat(_yargs.args);
+};
+
+exports["default"] = _default;
