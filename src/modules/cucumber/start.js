82a00f275adcea66ddbf515979a1e4cabc0520bf
Abhishek Das <abhi.das2007das@gmail.com>
2019-10-20 10:30:00 +0100

    feat(createOutputDir): auto creation of output directory

--- src/modules/cucumber/start.js
+++ src/modules/cucumber/start.js
@@ -1,12 +1,16 @@
 import { spawn } from 'cross-spawn';
 import path from 'path';
 import getCucumberOptions from './getCucumberOptions';
+import createOutputDir from './createOutputDir';
 import { logger } from '../logger';
 
 const cucumber = path.join('node_modules', '.bin', 'cucumber-js');
 
 export default (options) => {
   const cucumberOptions = getCucumberOptions(options);
+
+  // In case the output directory mentioned in the options is not available, it will create one.
+  createOutputDir(options);
 
   logger.data(
     `\nCUCUMBER OPTIONS:
