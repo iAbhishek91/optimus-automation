99d0accc4c8629192726059dc021ec676c1127d0
Abhishek Das <abhi.das2007das@gmail.com>
2019-10-21 16:48:42 +0100

    feat(webdriver/actions): exposing actions from the module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import * as logger from './modules/logger';
+import actions from './modules/webdriverio/actions';
 import request from './modules/request';
 import utility from './utility';
 
-export { logger, request, utility };
+export {
+  logger,
+  request,
+  utility,
+  actions,
+};
